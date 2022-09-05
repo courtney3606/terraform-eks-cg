675620ea923b44e72b4b635356bd8583a11d6d7d
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-05 19:15:33 -0400

    update providers file

--- providers.tf
+++ providers.tf
@@ -12,7 +12,6 @@ terraform {
     }
   }
   }
-}
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
