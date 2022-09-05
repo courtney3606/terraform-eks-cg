ecd38c1736ff335d3f4010be2910a361f7769e67
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-05 17:32:00 -0400

    update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -4,7 +4,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = aws_eks_cluster.cg-cluster.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_role.arn
-  subnet_ids      = aws_subnet.eks-subnet[*].id
+  subnet_ids      = [aws_subnet.eks-subnet1.id, aws_subnet.eks-subnet2.id]
 
   scaling_config {
     desired_size = 2
