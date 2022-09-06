f4025a4f96b47f4c8babcf8f9c826c16f81829af
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-06 14:39:04 -0400

    update all files

--- variables.tf
+++ variables.tf
@@ -4,5 +4,13 @@ variable "region" {
     type = string
     default = "us-east-1"
 }
-variable "container_ips" {}
-variable "cluster_name" {}
+variable "container_ips"{
+    type= string
+    default = var.container_ips
+}
+
+variable "cluster_name"{
+    type = string
+    default = var.container_ips
+
+}
