562baa3ed8c917add3ad79a9cdad846f332bbe7e
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-09 14:35:50 -0400

    updated providers file

--- providers.tf
+++ providers.tf
@@ -17,3 +17,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 } 
+
+provider "aws" {
+  region = "us-east-1"
+}
