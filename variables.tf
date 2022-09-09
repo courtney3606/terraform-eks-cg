be3fe19bda97859c420a7af365898ac5b9fb323b
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-09 14:13:42 -0400

    updated variables file

--- variables.tf
+++ variables.tf
@@ -6,10 +6,12 @@ variable "region" {
 }
 variable "container_ips"{
     type= string
+    default = "aws_eks_node_group.eks-node-group[*].id"
 
 }
 
 variable "cluster_name"{
     type = string
+    default = "aws_eks_cluster.cg-cluster.name"
     
 }
