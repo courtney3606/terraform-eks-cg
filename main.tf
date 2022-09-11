b155238a8a97a1b9bde82abb40474b895bd93336
courtney3606 <104524956+courtney3606@users.noreply.github.com>
2022-09-11 13:36:32 -0400

    updated providers file

--- main.tf
+++ main.tf
@@ -4,4 +4,5 @@ module "eks" {
     source = "./eks"
     container_ips = var.container_ips
     cluster_name = var.cluster_name
+    aws_availability_zones = [ "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f" ]
 }
